2381056ed070555b12084953bac7cae9e4a1ed89
Dynastry Benson <81444153+Dynastry25@users.noreply.github.com>
2025-09-22 14:14:10 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,7 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors({
-  origin: '',
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
